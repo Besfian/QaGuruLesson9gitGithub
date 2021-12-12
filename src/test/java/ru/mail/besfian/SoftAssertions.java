7c656a5a34512fdf78a7116683f648830ea4021c
besfian <besfian@mail.ru>
2021-12-13 02:09:00 +0300

    Удаление лишних комментариев

--- src/test/java/ru/mail/besfian/SoftAssertions.java
+++ src/test/java/ru/mail/besfian/SoftAssertions.java
@@ -10,16 +10,12 @@ import static com.codeborne.selenide.Selenide.open;
 public class SoftAssertions {
     @Test
     void softAssertions() {
-        // - Откройте страницу Selenide в Github
         open("https://github.com/search?q=selenide");
 
-        // - Перейдите в раздел Wiki проекта
         $(byText("Wikis")).click();
 
-        // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $("[title=SoftAssertions]").shouldHave(text("SoftAssertions"));
 
-        // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
         $("[title=SoftAssertions]").click();
         $(".markdown-body").shouldHave(text("com.codeborne.selenide.junit5.SoftAssertsExtension"));
 
